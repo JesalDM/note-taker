74675edb222d663078ba9baa7220cac1adb9e8c5
Jesal Mehta <jesaldmehta@gmail.com>
2020-12-06 22:45:29 -0500

    Installed the uuid npm for generating random id. Added the API route using POST method for saveNote and tested that it works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
  const notes = require('../db/db.json');
- console.log(notes);
+ const { v4: uuidv4 } = require('uuid');
  
  module.exports = function(app){
 
     app.get('/api/notes', function(req, res){
-        console.log("Retrieving note list");
         res.json(notes);
     });
+
+    app.post('/api/notes', function(req, res){
+        const newNote = req.body;
+        newNote.id= uuidv4();
+        notes.push(newNote);
+        res.json(notes);
+    })
  }
