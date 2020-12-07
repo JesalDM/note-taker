3aa88bfbe68b31763757cdd1400f39615fe9d6fe
Jesal Mehta <jesaldmehta@gmail.com>
2020-12-07 00:30:19 -0500

    Added the API route for deleteNote and tested that it deletes the note from db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,11 @@
- const notes = require('../db/db.json');
+ let notes = require('../db/db.json');
  const { v4: uuidv4 } = require('uuid');
+ const fs = require('fs');
+ const path = require('path');
+
+ 
  
  module.exports = function(app){
-
     app.get('/api/notes', function(req, res){
         res.json(notes);
     });
@@ -12,5 +15,22 @@
         newNote.id= uuidv4();
         notes.push(newNote);
         res.json(notes);
+    })
+
+    app.delete('/api/notes/:id', function(req, res){
+       const noteId = req.params.id;
+       console.log(__dirname);
+       let data = fs.readFileSync(path.join(__dirname, '../db/db.json'), "utf-8");
+       data = JSON.parse(data);
+       for (let i=0; i<data.length; i++){
+         if(data[i].id === noteId){
+             data.splice(i,1);
+         }
+       }
+       fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err)=>{
+           if (err) throw error;
+       })
+       notes = data;
+       res.json(notes)  
     })
  }
